05c3c796af0c64fb7fab54b4ae3a5174b0a077b2
Jakob0403 <Jakobjj0403@gmail.com>
2017-11-09 15:57:27 +0100

    Add files via upload

--- positivenumber.java
+++ positivenumber.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class positivenumber {
   public static void main(String[] args) {
-    Scanner reader = new Scanner(reader.nextLine());
+    Scanner reader = new Scanner(System.in);
     System.out.println("Hello!");
     System.out.println("Please choose a number.");
 
-    int choice = Integer.parseInt(reader.nextLine);
+    int choice = Integer.parseInt(reader.nextLine());
 
-    System.out.println("You chose " +  choice " as your number.");
+    System.out.println("You chose " +  choice + " as your number.");
 
     if (choice > 0) {
       System.out.println("Your chosen number is positive.");
